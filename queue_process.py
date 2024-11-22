96697769372799b7b5b365aafc2bfb57582ed7b7
DinisB <dinisbarroso.dev@gmail.com>
2024-11-22 11:36:29 +0000

    third commit

--- queue_process.py
+++ queue_process.py
@@ -1,17 +1,11 @@
 from collections import deque
-def reverse(stack, stacktoadd):
-    while stack:
-        popped = stack.pop()
-        stacktoadd.append(popped)
 stack = deque()
-stacktoadd = deque()
 palavras = input()
 palavras = palavras.split(" ")
 for i in range(len(palavras)):
-    stack.append(palavras[i])
-reverse(stack,stacktoadd)
-print(stacktoadd)
-while stacktoadd:
-    popped = stacktoadd.pop()
+    stack.appendleft(palavras[i])
+print(stack)
+while stack:
+    popped = stack.pop()
     if "a" in popped:
         print(popped)
